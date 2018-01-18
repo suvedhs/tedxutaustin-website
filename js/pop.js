fc266a3cd61393412ba293362bb51eff15055390
Suvedh Srikanth <suvedhs@gmail.com>
2018-01-18 01:35:54 -0600

    finished the popups besides fades.

--- js/pop.js
+++ js/pop.js
@@ -6,3 +6,51 @@ function daron() {
     var popup = document.getElementById("daron-popup");
     popup.classList.toggle("show");
 }
+function david() {
+    var popup = document.getElementById("david-popup");
+    popup.classList.toggle("show");
+}
+function doreen() {
+    var popup = document.getElementById("doreen-popup");
+    popup.classList.toggle("show");
+}
+function elizabeth() {
+    var popup = document.getElementById("elizabeth-popup");
+    popup.classList.toggle("show");
+}
+function henry() {
+    var popup = document.getElementById("henry-popup");
+    popup.classList.toggle("show");
+}
+function jake() {
+    var popup = document.getElementById("jake-popup");
+    popup.classList.toggle("show");
+}
+function just() {
+    var popup = document.getElementById("just-popup");
+    popup.classList.toggle("show");
+}
+function karen() {
+    var popup = document.getElementById("karen-popup");
+    popup.classList.toggle("show");
+}
+function mitchell() {
+    var popup = document.getElementById("mitchell-popup");
+    popup.classList.toggle("show");
+}
+function raj() {
+    var popup = document.getElementById("raj-popup");
+    popup.classList.toggle("show");
+}
+function ricco() {
+    var popup = document.getElementById("ricco-popup");
+    popup.classList.toggle("show");
+}
+function shauna() {
+    var popup = document.getElementById("shauna-popup");
+    popup.classList.toggle("show");
+}
+function thea() {
+    var popup = document.getElementById("thea-popup");
+    popup.classList.toggle("show");
+}
