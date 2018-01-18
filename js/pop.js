e3cd70578724e9bd4fdc065fc57e5d7f6e21307c
Suvedh Srikanth <suvedhs@gmail.com>
2018-01-18 01:16:25 -0600

    finally got a somewhat working demo of the speaker popup with scrolling.

--- js/pop.js
+++ js/pop.js
@@ -2,3 +2,7 @@ function annie() {
     var popup = document.getElementById("annie-popup");
     popup.classList.toggle("show");
 }
+function daron() {
+    var popup = document.getElementById("daron-popup");
+    popup.classList.toggle("show");
+}
