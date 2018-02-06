567ca725954d0303d0b3e456e3a3de1da1d44582
Suvedh Srikanth <suvedhs@gmail.com>
2018-02-05 19:47:29 -0600

    added popups and further fixed schedule

--- js/pop.js
+++ js/pop.js
@@ -1,56 +1,84 @@
 function annie() {
     var popup = document.getElementById("annie-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function daron() {
     var popup = document.getElementById("daron-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function david() {
     var popup = document.getElementById("david-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function doreen() {
     var popup = document.getElementById("doreen-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function elizabeth() {
     var popup = document.getElementById("elizabeth-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function henry() {
     var popup = document.getElementById("henry-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function jake() {
     var popup = document.getElementById("jake-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function just() {
     var popup = document.getElementById("just-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function karen() {
     var popup = document.getElementById("karen-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function mitchell() {
     var popup = document.getElementById("mitchell-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function raj() {
     var popup = document.getElementById("raj-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function ricco() {
     var popup = document.getElementById("ricco-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function shauna() {
     var popup = document.getElementById("shauna-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
 function thea() {
     var popup = document.getElementById("thea-popup");
     popup.classList.toggle("show");
+    var body = document.body;
+    body.classList.toggle("stop-scroll");
 }
